3fd3069986fe28dceee109cd26a60c928a7e4acc
Hamza Butt <hamzambutt1@gmail.com>
2021-05-07 23:30:10 +0100

    navbar now hides when user scrolls down, helpers added to enable use of debounce

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { debounce } from '../helpers/helpers.js'
+
 
 const Navbar = () => {
 
-  return <h1>Navbar</h1>
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+  const [visible, setVisible] = useState(true)
+
+  const navbarStyles = {
+    position: 'fixed',
+    height: '60px',
+    width: '100%',
+    backgroundColor: 'grey',
+    textAlign: 'center',
+    transition: 'top 0.6s',
+  }
+
+  const handleScroll = debounce(() => {
+    //find current scroll position
+    const currentScrollPos = window.pageYOffset
+
+    //set state based on location info
+    setVisible((prevScrollPos > currentScrollPos && prevScrollPos - currentScrollPos > 70) || currentScrollPos < 10)
+
+    //set state to new scroll positions
+    setPrevScrollPos(currentScrollPos)
+
+  }, 100)
   
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+
+    return  () => window.removeEventListener('scroll', handleScroll)
+  }, [prevScrollPos, visible, handleScroll])
+
+  return <div className="navbar" style={{ ...navbarStyles, top: visible ? '0' : '-60px' }}>Home</div>
+
 }
 
 export default Navbar
