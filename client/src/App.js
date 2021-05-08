2296b4f8afa71032e8832e9913c4e8a8394f47c9
Hamza Butt <hamzambutt1@gmail.com>
2021-05-08 23:13:12 +0100

    devicons added and they render on page, still require colour change and adjust div containers for them

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import About from './components/About'
 
 
 
@@ -10,6 +11,7 @@ const App = () => {
     <>
       <Navbar />
       <Hero />
+      <About />
     </>
   )
 }
