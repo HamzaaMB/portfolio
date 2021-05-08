3013e7e59807e698ddfdbbb8af2d75959df3e124
Hamza Butt <hamzambutt1@gmail.com>
2021-05-08 21:29:52 +0100

    bounce effect for scroll added, css changes to hero containers

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -7,10 +7,17 @@ const Hero = () => {
 
   return (
     <>
-      <div className="hero-container">
-        <h1 className="hero-name">hamza butt</h1>
-        <h2 className="hero-title">software engineer</h2>
-      </div>
+      <section className="hero">
+        <div className="hero-container">
+          <h1 className="hero-name">hamza butt</h1>
+          <h2 className="hero-title">software engineer</h2>
+        </div>
+        <div className="hero-scroll">
+          <div className="arrow bounce">
+            <a className="arrow-down" href="/">scroll</a>
+          </div>
+        </div>
+      </section>
     </>
 
   )
