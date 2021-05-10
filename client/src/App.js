f405507dd44148a7563987bb56ec50fdfac6735b
Hamza Butt <hamzambutt1@gmail.com>
2021-05-10 21:54:33 +0100

    projects component created, added to app.js and tested for rendering

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
+import Projects from './components/Projects'
 
 
 
@@ -12,6 +13,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Projects />
     </>
   )
 }
