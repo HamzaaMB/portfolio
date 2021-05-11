8eb39e8a9c8c48903c97b2de8d73e38a33b802a9
Hamza Butt <hamzambutt1@gmail.com>
2021-05-11 02:40:44 +0100

    carousel updated

--- client/src/components/Slideshow.js
+++ client/src/components/Slideshow.js
@@ -3,7 +3,7 @@ import { Slide }  from 'react-slideshow-image'
 
 
 const slideImages = [
-  'https://www.google.com/imgres?imgurl=https%3A%2F%2Fimg.etimg.com%2Fthumb%2Fmsid-56500181%2Cwidth-640%2Cresizemode-4%2Cimgsize-276013%2Fgoogle-sky.jpg&imgrefurl=https%3A%2F%2Feconomictimes.indiatimes.com%2Ftech-life%2F7-little-known-google-products-and-services%2Fgoogle-sky%2Fslideshow%2F56500181.cms&tbnid=IA0VhC6BENQK3M&vet=12ahUKEwiOoqWzi8DwAhUMcxoKHT7pC3sQMygIegUIARDZAQ..i&docid=wu7BnTYwevFOsM&w=640&h=480&q=google%20image%20sky&ved=2ahUKEwiOoqWzi8DwAhUMcxoKHT7pC3sQMygIegUIARDZAQ',
+  'images/slide_2.jpg',
   'images/slide_3.jpg',
   'images/slide_4.jpg'
 ]
