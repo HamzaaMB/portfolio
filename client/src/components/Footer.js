81fce25474dcc77137024652bd4184638b8c654a
Hamza Butt <hamzambutt1@gmail.com>
2021-05-13 20:05:03 +0100

    interest and contact containers filled, pictures added with effects and further css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -5,8 +5,42 @@ const Footer = () => {
   return (
     <>
       <footer id="contact">
-        <div className="interest-container">interest</div>
-        <div className="contact-container">contact</div>
+        <div className="contact-top"><h2 className="interest-title">Interests</h2><h2 className="contact-title">contact</h2></div>
+        <div className="contact-bottom">
+          <div className="interest-container">
+            <div className="image-box-container">
+              <div className="image-box">
+                <img src='./screenshots/travel.png.jpg' alt="travel" />
+                <div className="text">
+                  <h4>travel</h4>
+                </div>
+              </div>
+              <div className="image-box">
+                <img src='./screenshots/photography.png' alt="photography" />
+                <div className="text">
+                  <h4>photography</h4>
+                </div>
+              </div>
+              <div className="image-box">
+                <img src='./screenshots/gaming.jpeg' alt="gaming" />
+                <div className="text">
+                  <h4>gaming</h4>
+                </div>
+              </div>
+              <div className="image-box">
+                <img src='./screenshots/reading.jpeg' alt="reading" />
+                <div className="text">
+                  <h4>reading</h4>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="contact-container">
+            <div className="email">email</div>
+            <div className="linkedin">linkedin</div>
+            <div className="github">github</div>
+          </div>
+        </div>
       </footer>
     </>
   )
